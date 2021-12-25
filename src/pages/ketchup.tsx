e474365deb9dd7acd64de476205d3180291b32c5
Erick Hilda <erickh.andreas@gmail.com>
2021-12-26 02:17:11 +0700

    add layout, global css rule, and some prettier config

---
+++ src/pages/ketchup.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import Layout from "../components/layout";
+
+const Topping: NextPage = () => {
+  return (
+    <Layout>
+      <section className="mt-10">
+        <h2 className="text-xl font-semibold text-slate-800 dark:text-slate-200">
+          Story
+        </h2>
+
+        <div className="mt-2">
+          <p className="text-slate-800 dark:text-slate-200">
+            &quot;I confess that when this all started, you were like a picture
+            out of focus to me. And it took time for my eyes to adjust to you,
+            to make sense of you, to really recognize you.&quot;
+          </p>
+
+          <br />
+
+          <p className="text-sm text-slate-800 dark:text-slate-200">
+            <a
+              href="https://twosentencestories.com/vision"
+              className="underline"
+            >
+              Vision
+            </a>
+            , a two sentence story
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default Topping;

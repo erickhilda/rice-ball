f38b15cc6ad6c7c599ff18aeffd159fccc55f468
Erick Hilda <erick.hilda@ajaib.co.id>
2022-02-26 20:32:58 +0700

    use next-theme for themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Meta from "components/meta";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Meta />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 

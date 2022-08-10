87fdb6bc5066cfde7357245cef9bf0d71067fd2d
Erick Hilda <erick.hilda@ajaib.co.id>
2022-08-10 14:25:53 +0700

    chore: add custom document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+  NextScript,
+  Main,
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+} from "next/document";
+import Meta from "components/meta";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
